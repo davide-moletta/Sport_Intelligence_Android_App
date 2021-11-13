4a335957e21ea05bc96b4413329ee043528aadfd
davide-moletta <davide.moletta@studenti.unitn.it>
2021-11-13 15:51:50 +0100

    SearchResultFragment finished. Working on remote connection.

--- app/src/main/java/com/example/sportintelligencetesimolettadavide/SearchResultFragment.java
+++ app/src/main/java/com/example/sportintelligencetesimolettadavide/SearchResultFragment.java
@@ -26,11 +26,12 @@ public class SearchResultFragment extends Fragment {
     private static final String FILE_FAVOURITE_NAME = "favouriteMatches.txt";
 
     ImageView back, star;
-    TextView tournamentName, tournamentInfo, firstPlayer, result, secondPlayer, duration, matchStatsLabel, setStatsLabel, setHistoryLabel, quotesLabel;
+    TextView tournamentName, tournamentInfo, firstPlayer, result, secondPlayer, duration, subTitleSetStats, subTitleHistory,
+            matchStatsLabel, setStatsLabel, setHistoryLabel, quotesLabel;
     Spinner filterSpinner, setSpinner;
-    ConstraintLayout matchStatsLayout, setStatsLayout, setHistoryLayout, quotesLayout;
+    ConstraintLayout matchInfoLayout, matchStatsLayout, setStatsLayout, setHistoryLayout, quotesLayout;
 
-    boolean favourite = false;
+    boolean favourite = false, matchStatsInfo = true, setStatInfo = true, setHistoryInfo = true, quotesInfo = true;
 
     NavController navController;
     FileOperations fileFilters, fileFavouriteMatches;
@@ -66,6 +67,7 @@ public class SearchResultFragment extends Fragment {
         //Ottiene l'id del match selezionato
         int matchId = SearchResultFragmentArgs.fromBundle(getArguments()).getMatchId();
 
+        //Ottiene l'id degli elementi del file xml
         back = view.findViewById(R.id.back);
         star = view.findViewById(R.id.star);
         tournamentName = view.findViewById(R.id.tournamentName);
@@ -76,10 +78,13 @@ public class SearchResultFragment extends Fragment {
         duration = view.findViewById(R.id.duration);
         filterSpinner = view.findViewById(R.id.spinnerFilter);
         setSpinner = view.findViewById(R.id.setSpinner);
+        subTitleHistory = view.findViewById(R.id.subTitleHistory);
+        subTitleSetStats = view.findViewById(R.id.subTitleSetStats);
         matchStatsLabel = view.findViewById(R.id.matchStatsLabel);
         setStatsLabel = view.findViewById(R.id.setStatsLabel);
         setHistoryLabel = view.findViewById(R.id.setHistoryLabel);
         quotesLabel = view.findViewById(R.id.quotesLabel);
+        matchInfoLayout = view.findViewById(R.id.matchInfo);
         matchStatsLayout = view.findViewById(R.id.matchStats);
         setStatsLayout = view.findViewById(R.id.setStats);
         setHistoryLayout = view.findViewById(R.id.setHistory);
@@ -106,6 +111,9 @@ public class SearchResultFragment extends Fragment {
                 favourite = true;
             }
         }
+        if (!favourite) {
+            star.setBackgroundResource(R.drawable.ic_emptystar);
+        }
 
         //Ottiene i dati del match dal database
         match = neo4J.fetchAllDataFromId(matchId);
@@ -121,18 +129,6 @@ public class SearchResultFragment extends Fragment {
         setsHistory = match.getSetsHistory();
         setsTiebreaks = match.getSetsTiebreaks();
 
-        if (ObjectListToString(matchStat).equals("")) {
-            matchStatsLayout.setVisibility(View.INVISIBLE);
-        } else {
-            matchStatsLabel.setText(ObjectListToString(matchStat));
-        }
-
-        if (ObjectListToString(quotes).equals("")) {
-            quotesLabel.setVisibility(View.INVISIBLE);
-        } else {
-            quotesLabel.setText(ObjectListToString(quotes));
-        }
-
         for (List list : setsStat) {
             setStat.add(ObjectListToString(list));
         }
@@ -146,10 +142,6 @@ public class SearchResultFragment extends Fragment {
             setTiebreaks.add(ObjectListToString(list));
         }
 
-
-        //CODICE PER MOSTRARE I DATI DEI SET
-
-
         //Calcola il numero di set e crea il vettore da inserire nell'adapter
         int numberOfSets = 0;
         for (String set : setGames) {
@@ -157,7 +149,6 @@ public class SearchResultFragment extends Fragment {
                 numberOfSets++;
             }
         }
-
         String[] sets = new String[numberOfSets];
         for (int i = 0; i < numberOfSets; i++) {
             int j = i + 1;
@@ -168,6 +159,41 @@ public class SearchResultFragment extends Fragment {
         setAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         setSpinner.setAdapter(setAdapter);
 
+        //Salva le informazioni dei set in un vettore per la corretta visualizzazione dei dati tramite spinner
+        String[] stat = setListToString(setStat, numberOfSets);
+        String[] history = setHistoryStringBuilder(setGames, setFifteens, setTiebreaks, numberOfSets);
+
+        //Controlla se sono presenti dati ricevuti dal database (per match vecchi i dati non vengono salvati)
+        //se non sono presenti dati nasconde le relative finestre
+        if (ObjectListToString(matchStat).equals("")) {
+            matchStatsInfo = false;
+            matchStatsLayout.setVisibility(View.INVISIBLE);
+        } else {
+            matchStatsLabel.setText(ObjectListToString(matchStat));
+        }
+
+        if (ObjectListToString(quotes).equals("no data")) {
+            quotesInfo = false;
+            quotesLayout.setVisibility(View.INVISIBLE);
+        } else {
+            quotesLabel.setText(ObjectListToString(quotes));
+        }
+
+        if (stat.length == 0) {
+            setStatInfo = false;
+            setStatsLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (history.length == 0) {
+            setHistoryInfo = false;
+            setHistoryLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (!setStatInfo && !setHistoryInfo) {
+            setSpinner.setVisibility(View.INVISIBLE);
+        }
+
+        //Assegna i dati raccolti alle verie textView
         tournamentName.setText(editionAndDate);
         tournamentInfo.setText(tournamentInfoString);
         firstPlayer.setText(match.getFirstPlayer());
@@ -175,13 +201,55 @@ public class SearchResultFragment extends Fragment {
         secondPlayer.setText(match.getSecondPlayer());
         duration.setText(match.getDuration());
 
-        neo4J.close();
 
         //Imposta on Listener sullo spinner dei filtri per controllare quando viene selezionato un elemento diverso
         filterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //In base al filtro selezionato rimuove o aggiunge i campi precedentemente selezionati
+                if (i == 0) {
+                    matchInfoLayout.setVisibility(View.VISIBLE);
 
+                    if (matchStatsInfo) {
+                        matchStatsLayout.setVisibility(View.VISIBLE);
+                    }
+                    if (setStatInfo) {
+                        setStatsLayout.setVisibility(View.VISIBLE);
+                    }
+                    if (setHistoryInfo) {
+                        setHistoryLayout.setVisibility(View.VISIBLE);
+                    }
+                    if (quotesInfo) {
+                        quotesLayout.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    String filter = filters[i - 1];
+                    matchInfoLayout.setVisibility(View.INVISIBLE);
+                    matchStatsLayout.setVisibility(View.INVISIBLE);
+                    setStatsLayout.setVisibility(View.INVISIBLE);
+                    setHistoryLayout.setVisibility(View.INVISIBLE);
+                    quotesLayout.setVisibility(View.INVISIBLE);
+
+                    String[] filterTypes = filter.split(":")[1].split("-");
+
+                    for (String type : filterTypes) {
+                        if (type.equals("matchInfo")) {
+                            matchInfoLayout.setVisibility(View.VISIBLE);
+                        }
+                        if (type.equals("matchStats") && matchStatsInfo) {
+                            matchStatsLayout.setVisibility(View.VISIBLE);
+                        }
+                        if (type.equals("setStats") && setStatInfo) {
+                            setStatsLayout.setVisibility(View.VISIBLE);
+                        }
+                        if (type.equals("setHistory") && setHistoryInfo) {
+                            setHistoryLayout.setVisibility(View.VISIBLE);
+                        }
+                        if (type.equals("quotes") && quotesInfo) {
+                            quotesLayout.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
             }
 
             @Override
@@ -194,6 +262,30 @@ public class SearchResultFragment extends Fragment {
         setSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //In base al set selezionato aggiorna le informazioni nelle rispettive textview
+                if (!setStatInfo || !setHistoryInfo) {
+                    if (setStatInfo) {
+                        int j = i + 1;
+                        String statSubTitle = "STATISTICHE SET " + j;
+                        subTitleSetStats.setText(statSubTitle);
+                        setStatsLabel.setText(stat[i]);
+                    } else if (setHistoryInfo) {
+                        int j = i + 1;
+                        String historySubTitle = "STORICO SET " + j;
+                        subTitleHistory.setText(historySubTitle);
+                        setHistoryLabel.setText(history[i]);
+                    }
+                } else {
+                    int j = i + 1;
+                    String statSubTitle = "STATISTICHE SET " + j;
+                    String historySubTitle = "STORICO SET " + j;
+
+                    subTitleSetStats.setText(statSubTitle);
+                    subTitleHistory.setText(historySubTitle);
+
+                    setStatsLabel.setText(stat[i]);
+                    setHistoryLabel.setText(history[i]);
+                }
 
             }
 
@@ -217,7 +309,10 @@ public class SearchResultFragment extends Fragment {
         });
 
         //Imposta un OnClickListener per permettere la navigazione verso il fragment precedente
-        back.setOnClickListener(v -> navController.navigateUp());
+        back.setOnClickListener(v -> {
+            neo4J.close();
+            navController.navigateUp();
+        });
     }
 
     //Trasforma una lista di oggetti in una stringa da inserire poi nelle label
@@ -232,5 +327,62 @@ public class SearchResultFragment extends Fragment {
             fullString = new StringBuilder(fullString.substring(0, fullString.length() - 2));
         }
         return fullString.toString();
+    }
+
+    private String[] setListToString(List<String> listToConvert, int setNumber) {
+        String[] sets = new String[setNumber];
+        StringBuilder fullString;
+
+        for (int i = 0; i < setNumber; i++) {
+            fullString = new StringBuilder();
+            if (!listToConvert.get(i).equals("")) {
+                String[] rows = listToConvert.get(i).split("\n");
+                for (String row : rows) {
+                    fullString.append(row).append("\n");
+                }
+                if (!fullString.toString().equals("")) {
+                    fullString = new StringBuilder(fullString.substring(0, fullString.length() - 2));
+                }
+                sets[i] = fullString.toString();
+            }
+        }
+
+        return sets;
+    }
+
+    private String[] setHistoryStringBuilder(List<String> games, List<String> fifteens, List<String> tiebreaks, int setNumber) {
+        String[] sets = new String[setNumber];
+        StringBuilder fullString;
+
+        for (int i = 0; i < setNumber; i++) {
+            fullString = new StringBuilder();
+            if (!games.get(i).equals("") && !fifteens.get(i).equals("")) {
+                String[] setGames = games.get(i).split("\n");
+                String[] setFifteens = fifteens.get(i).split("\n");
+
+
+                for (int j = 0; j < setFifteens.length; j++) {
+                    fullString.append("\n").append(setGames[j]).append("\n").append(setFifteens[j]);
+                }
+                if (setGames.length > setFifteens.length) {
+                    fullString.append("\n\n").append(setGames[setGames.length - 1]);
+                }
+                fullString.append("\n");
+
+                if (!tiebreaks.get(i).equals("no data")) {
+                    fullString.append("\n").append("TIEBREAK").append("\n\n");
+
+                    String[] setTiebreaks = tiebreaks.get(i).split("\n");
+                    for (String setTiebreak : setTiebreaks) {
+                        fullString.append(setTiebreak).append("\n");
+                    }
+                }
+            }
+            if (!fullString.toString().equals("")) {
+                fullString = new StringBuilder(fullString.substring(1, fullString.length() - 1));
+            }
+            sets[i] = fullString.toString();
+        }
+        return sets;
     }
 }
